23c5cb15e27b8faa7ac7abbd16d945c08ff262b8
Benjamin Reyes <b.reyesordenes@uandresbello.edu>
2026-08-17 11:57:38 -0400

    Refactor: modulariza DWEB.html a plantillas PHP (header, sidebar, footer, index)

---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer>
+  <div class="container flex-footer">
+    <p class="mb-0">© 2026 Sala Táctica</p>
+    <a href="#">Contacto</a>
+  </div>
+</footer>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+</body>
+</html>
