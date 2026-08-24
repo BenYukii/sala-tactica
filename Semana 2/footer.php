3f006d01883f9afa92e891e83f6572b5bd0c1504
Benjamin Reyes <b.reyesordenes@uandresbello.edu>
2026-08-24 16:22:20 -0400

    feat(layout): carga dom.js en el pie de página

--- Semana 2/footer.php
+++ Semana 2/footer.php
@@ -7,6 +7,7 @@
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<script src="dom.js"></script>
 
 </body>
 </html>
